393d016a378bd14ee5d8e58e721720f81458a3fd
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-08 01:23:34 +0530

    ✅ Java Substring Comparisons | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-07

--- hackerrank/easy/java-string-reverse/solution.java
+++ hackerrank/easy/java-string-reverse/solution.java
@@ -1,31 +1,21 @@
-import java.io.*;
-import java.util.*;
 
-public class Solution {
 
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        String A = sc.next();
-        
-        boolean isPalindrome = true;
-        
-        for (int i = 0; i < A.length() / 2; i++) {
-            if (A.charAt(i) != A.charAt(A.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+    public static String getSmallestAndLargest(String s, int k) {
+    String smallest = s.substring(0, k);
+    String largest = s.substring(0, k);
+
+    for (int i = 1; i <= s.length() - k; i++) {
+        String current = s.substring(i, i + k);
+
+        if (current.compareTo(smallest) < 0) {
+            smallest = current;
         }
-        
-        if (isPalindrome) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
+
+        if (current.compareTo(largest) > 0) {
+            largest = current;
         }
-        
-        sc.close();
     }
+
+    return smallest + "\n" + largest;
 }
-
-
 
