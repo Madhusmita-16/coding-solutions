4177ff43b0c872db9ea24a465f43dab38e6d8b4a
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-08 01:34:29 +0530

    ✅ Java Inheritance I | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-07

--- hackerrank/easy/java-inheritance-1/solution.java
+++ hackerrank/easy/java-inheritance-1/solution.java
@@ -1,23 +1,13 @@
 
-class MyCalculator {
 
-    public long power(int n, int p) throws Exception {
 
-        if (n < 0 || p < 0) {
-            throw new Exception("n or p should not be negative.");
-        }
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
 
-        if (n == 0 && p == 0) {
-            throw new Exception("n and p should not be zero.");
-        }
-
-        long result = 1;
-
-        for (int i = 0; i < p; i++) {
-            result *= n;
-        }
-
-        return result;
+    void sing() {
+        System.out.println("I am singing");
     }
 }
 
