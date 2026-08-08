f865ec305f2942f2d918ff2cb6c4f8882b60b4f3
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-08 14:23:34 +0530

    ✅ Java Hashset | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-08

--- hackerrank/easy/java-hashset/solution.java
+++ hackerrank/easy/java-hashset/solution.java
@@ -3,7 +3,7 @@
 HashSet<String> set = new HashSet<>();
 
 for (int i = 0; i < t; i++) {
-    set.add(pair_left[i] + "#" + pair_right[i]);
+    set.add(pair_left[i] + " " + pair_right[i]);
     System.out.println(set.size());
 }
 
