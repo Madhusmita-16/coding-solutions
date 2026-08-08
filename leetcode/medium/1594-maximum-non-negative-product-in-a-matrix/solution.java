101e40fbd3e96a4d31822e0bf27c455af3cb4ced
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-08 18:06:27 +0530

    ✅ Maximum Non Negative Product in a Matrix | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-08

---
+++ leetcode/medium/1594-maximum-non-negative-product-in-a-matrix/solution.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    public int maxProductPath(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        long[][] max = new long[m][n];
+        long[][] min = new long[m][n];
+
+        max[0][0] = grid[0][0];
+        min[0][0] = grid[0][0];
+
+        // First row
+        for (int j = 1; j < n; j++) {
+            max[0][j] = max[0][j - 1] * grid[0][j];
+            min[0][j] = min[0][j - 1] * grid[0][j];
+        }
+
+        // First column
+        for (int i = 1; i < m; i++) {
+            max[i][0] = max[i - 1][0] * grid[i][0];
+            min[i][0] = min[i - 1][0] * grid[i][0];
+        }
+
+        // Remaining cells
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+
+                long value = grid[i][j];
+
+                long maxPrev = Math.max(max[i - 1][j], max[i][j - 1]);
+                long minPrev = Math.min(min[i - 1][j], min[i][j - 1]);
+
+                long maxProduct = Math.max(
+                    maxPrev * value,
+                    minPrev * value
+                );
+
+                long minProduct = Math.min(
+                    maxPrev * value,
+                    minPrev * value
+                );
+
+                max[i][j] = maxProduct;
+                min[i][j] = minProduct;
+            }
+        }
+
+        // If maximum product is negative, no non-negative
+        // product exists.
+        if (max[m - 1][n - 1] < 0) {
+            return -1;
+        }
+
+        int MOD = 1_000_000_007;
+
+        return (int) (max[m - 1][n - 1] % MOD);
+    }
+}
