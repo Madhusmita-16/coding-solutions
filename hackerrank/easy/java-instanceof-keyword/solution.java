655d5f69dfb83a3942c6b803eb9cd1fe073f06f0
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-08 16:42:38 +0530

    ✅ Java Instanceof keyword | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-08

--- hackerrank/easy/java-instanceof-keyword/solution.java
+++ hackerrank/easy/java-instanceof-keyword/solution.java
@@ -1,43 +1,38 @@
-import java.io.*;
 import java.util.*;
 
-class Vehicle {
-    String type;
 
-    void setType(String type) {
-        this.type = type;
-    }
+class Student{}
+class Rockstar{   }
+class Hacker{}
 
-    String getType() {
-        return type;
-    }
-}
 
-class Cycle extends Vehicle {
-    Cycle() {
-        setType("cycle");
-    }
+public class InstanceOFTutorial{
+    
+   static String count(ArrayList mylist){
+      int a = 0,b = 0,c = 0;
+      for(int i = 0; i < mylist.size(); i++){
+         Object element=mylist.get(i);
+         if(element instanceof Student)
+            a++;
+         if(element instanceof Rockstar)
+            b++;
+         if(element instanceof Hacker)
+            c++;
+      }
+      String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+      return ret;
+   }
 
-    void print() {
-        System.out.println("My ancestor is a cycle who is a vehicle with pedals.");
-    }
-}
-
-class MotorCycle extends Cycle {
-    MotorCycle() {
-        setType("motorcycle");
-    }
-
-    void print() {
-        System.out.println("Hello I am a " + getType() + ", I am a cycle with an engine.");
-        super.print();
-    }
-}
-
-public class Solution {
-
-    public static void main(String[] args) {
-        MotorCycle m = new MotorCycle();
-        m.print();
-    }
+   public static void main(String []args){
+      ArrayList mylist = new ArrayList();
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      for(int i=0; i<t; i++){
+         String s=sc.next();
+         if(s.equals("Student"))mylist.add(new Student());
+         if(s.equals("Rockstar"))mylist.add(new Rockstar());
+         if(s.equals("Hacker"))mylist.add(new Hacker());
+      }
+      System.out.println(count(mylist));
+   }
 }
