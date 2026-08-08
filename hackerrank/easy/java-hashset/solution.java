1d80397b8e6ee2f2d52f36879b16a9be0227019a
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-08 14:52:55 +0530

    ✅ Java Hashset | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-08

--- hackerrank/easy/java-hashset/solution.java
+++ hackerrank/easy/java-hashset/solution.java
@@ -3,44 +3,21 @@ import java.util.*;
 
 public class Solution {
 
-    static class Pair {
-        String first;
-        String second;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
 
-        Pair(String first, String second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        public int hashCode() {
-            return Objects.hash(first, second);
-        }
-
-        public boolean equals(Object obj) {
-            if (!(obj instanceof Pair)) {
-                return false;
-            }
-
-            Pair p = (Pair) obj;
-            return first.equals(p.first) && second.equals(p.second);
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int t = scan.nextInt();
-        HashSet<Pair> set = new HashSet<>();
+        Set<String> pairs = new HashSet<>();
+        StringBuilder output = new StringBuilder();
 
         for (int i = 0; i < t; i++) {
-            String a = scan.next();
-            String b = scan.next();
-
-            set.add(new Pair(a, b));
-
-            System.out.println(set.size());
+            String line = br.readLine().trim();  // trim kills \r and stray spaces
+            String[] parts = line.split(" ");
+            String key = parts[0] + " " + parts[1];
+            pairs.add(key);
+            output.append(pairs.size()).append("\n");
         }
 
-        scan.close();
+        System.out.print(output);
     }
 }
