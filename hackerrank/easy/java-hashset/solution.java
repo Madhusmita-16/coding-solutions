149dd9052237595f29ad9d42adce0cbc7b7a6f57
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-08 14:17:27 +0530

    ✅ Java Hashset | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-08

--- hackerrank/easy/java-hashset/solution.java
+++ hackerrank/easy/java-hashset/solution.java
@@ -1,36 +1,8 @@
-import java.util.*;
-import java.io.*;
 
-class Solution {
-    public static void main(String[] argh) {
 
-        Scanner in = new Scanner(System.in);
+HashSet<String> set = new HashSet<>();
 
-        int n = in.nextInt();
-        in.nextLine();
-
-        HashMap<String, String> phoneBook = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            String name = in.nextLine();
-            String phone = in.nextLine();
-
-            phoneBook.put(name, phone);
-        }
-
-        while (in.hasNextLine()) {
-            String name = in.nextLine();
-
-            if (phoneBook.containsKey(name)) {
-                System.out.println(name + "=" + phoneBook.get(name));
-            } else {
-                System.out.println("Not found");
-            }
-        }
-
-        in.close();
-    }
+for (int i = 0; i < t; i++) {
+    set.add(pair_left[i] + "#" + pair_right[i]);
+    System.out.println(set.size());
 }
-
-
-
