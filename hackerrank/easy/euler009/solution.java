b2d55148bb5e0b85cee869f3a0b9c40782be1bc9
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-09 15:02:50 +0530

    ✅ Project Euler #9: Special Pythagorean triplet | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-09

--- hackerrank/easy/euler009/solution.java
+++ hackerrank/easy/euler009/solution.java
@@ -1,28 +1,44 @@
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
         int t = in.nextInt();
 
         while (t-- > 0) {
+
             long n = in.nextLong();
+            long maxProduct = -1;
 
-            long sum = 0;
-            long a = 2;
-            long b = 8;
+            // a < b < c
+            // a can never be >= n/3
+            for (long a = 1; a < n / 3; a++) {
 
-            while (a <= n) {
-                sum += a;
-                long c = 4 * b + a;
-                a = b;
-                b = c;
+                long numerator = n * (n - 2 * a);
+                long denominator = 2 * (n - a);
+
+                // b must be an integer
+                if (numerator % denominator != 0) {
+                    continue;
+                }
+
+                long b = numerator / denominator;
+                long c = n - a - b;
+
+                if (b > a && b < c) {
+                    long product = a * b * c;
+                    maxProduct = Math.max(maxProduct, product);
+                }
             }
 
-            System.out.println(sum);
+            System.out.println(maxProduct);
         }
 
         in.close();
