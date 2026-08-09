9f0b67f15b308b5b408ddcd4e54ba874d01e6cd4
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-09 15:20:23 +0530

    ✅ Project Euler #17: Number to Words | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-09

--- hackerrank/easy/euler017/solution.java
+++ hackerrank/easy/euler017/solution.java
@@ -1,8 +1,61 @@
 import java.io.*;
 import java.util.*;
+import java.text.*;
 import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
+
+    static String[] ones = {
+        "", "One", "Two", "Three", "Four", "Five",
+        "Six", "Seven", "Eight", "Nine", "Ten",
+        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+        "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    static String[] tens = {
+        "", "", "Twenty", "Thirty", "Forty",
+        "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+    static String convert(long n) {
+
+        if (n == 0) {
+            return "Zero";
+        }
+
+        if (n < 20) {
+            return ones[(int)n];
+        }
+
+        if (n < 100) {
+            return tens[(int)(n / 10)] +
+                   (n % 10 == 0 ? "" : " " + ones[(int)(n % 10)]);
+        }
+
+        if (n < 1000) {
+            return ones[(int)(n / 100)] + " Hundred" +
+                   (n % 100 == 0 ? "" : " " + convert(n % 100));
+        }
+
+        if (n < 1000000) {
+            return convert(n / 1000) + " Thousand" +
+                   (n % 1000 == 0 ? "" : " " + convert(n % 1000));
+        }
+
+        if (n < 1000000000) {
+            return convert(n / 1000000) + " Million" +
+                   (n % 1000000 == 0 ? "" : " " + convert(n % 1000000));
+        }
+
+        if (n < 1000000000000L) {
+            return convert(n / 1000000000) + " Billion" +
+                   (n % 1000000000 == 0 ? "" : " " + convert(n % 1000000000));
+        }
+
+        return convert(n / 1000000000000L) + " Trillion" +
+               (n % 1000000000000L == 0 ? "" : " " + convert(n % 1000000000000L));
+    }
 
     public static void main(String[] args) {
 
@@ -12,19 +65,9 @@ public class Solution {
 
         while (t-- > 0) {
 
-            int n = in.nextInt();
+            long n = in.nextLong();
 
-            BigInteger number = BigInteger.valueOf(2).pow(n);
-
-            String value = number.toString();
-
-            int sum = 0;
-
-            for (int i = 0; i < value.length(); i++) {
-                sum += value.charAt(i) - '0';
-            }
-
-            System.out.println(sum);
+            System.out.println(convert(n));
         }
 
         in.close();
