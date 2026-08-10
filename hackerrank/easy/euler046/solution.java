87449faa0725fa715175b41e8a67983ab9bd53e4
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-10 15:49:00 +0530

    ✅ Project Euler #46: Goldbach's other conjecture | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-10

--- hackerrank/easy/euler046/solution.java
+++ hackerrank/easy/euler046/solution.java
@@ -6,143 +6,61 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static long triangular(long n) {
-        return n * (n + 1) / 2;
-    }
+    static boolean isPrime(long n) {
 
-    static long pentagonal(long n) {
-        return n * (3 * n - 1) / 2;
-    }
-
-    static long hexagonal(long n) {
-        return n * (2 * n - 1);
-    }
-
-    static boolean isTriangular(long x) {
-        if (x <= 0) {
+        if (n < 2) {
             return false;
         }
 
-        long d = 8 * x + 1;
-        long r = (long) Math.sqrt(d);
-
-        while ((r + 1) * (r + 1) <= d) {
-            r++;
+        if (n == 2) {
+            return true;
         }
 
-        while (r * r > d) {
-            r--;
-        }
-
-        return r * r == d;
-    }
-
-    static boolean isPentagonal(long x) {
-        if (x <= 0) {
+        if (n % 2 == 0) {
             return false;
         }
 
-        long d = 24 * x + 1;
-        long r = (long) Math.sqrt(d);
-
-        while ((r + 1) * (r + 1) <= d) {
-            r++;
+        for (long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
         }
 
-        while (r * r > d) {
-            r--;
-        }
-
-        return r * r == d && (1 + r) % 6 == 0;
-    }
-
-    static boolean isHexagonal(long x) {
-        if (x <= 0) {
-            return false;
-        }
-
-        long d = 8 * x + 1;
-        long r = (long) Math.sqrt(d);
-
-        while ((r + 1) * (r + 1) <= d) {
-            r++;
-        }
-
-        while (r * r > d) {
-            r--;
-        }
-
-        return r * r == d && (1 + r) % 4 == 0;
+        return true;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        long N = sc.nextLong();
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int t = sc.nextInt();
 
-        StringBuilder ans = new StringBuilder();
+        while (t-- > 0) {
 
-        /*
-         * 3 = Triangular
-         * 5 = Pentagonal
-         * 6 = Hexagonal
-         */
+            long n = sc.nextLong();
 
-        if ((a == 3 && b == 5) || (a == 5 && b == 3)) {
-
-            // Generate pentagonal numbers and check triangular.
-            for (long i = 1; ; i++) {
-
-                long p = pentagonal(i);
-
-                if (p >= N) {
-                    break;
-                }
-
-                if (isTriangular(p)) {
-                    ans.append(p).append('\n');
-                }
-            }
-
-        } else if ((a == 5 && b == 6) || (a == 6 && b == 5)) {
-
-            // Generate hexagonal numbers and check pentagonal.
-            for (long i = 1; ; i++) {
-
-                long h = hexagonal(i);
-
-                if (h >= N) {
-                    break;
-                }
-
-                if (isPentagonal(h)) {
-                    ans.append(h).append('\n');
-                }
-            }
-
-        } else if ((a == 3 && b == 6) || (a == 6 && b == 3)) {
+            int count = 0;
 
             /*
-             * Every hexagonal number is triangular.
-             * Therefore every hexagonal number below N
-             * satisfies the condition.
+             * N = prime + 2 * square
+             *
+             * Therefore:
+             *
+             * prime = N - 2 * i * i
+             *
+             * Try every possible square.
              */
-            for (long i = 1; ; i++) {
+            for (long i = 1; 2 * i * i < n; i++) {
 
-                long h = hexagonal(i);
+                long prime = n - 2 * i * i;
 
-                if (h >= N) {
-                    break;
+                if (isPrime(prime)) {
+                    count++;
                 }
-
-                ans.append(h).append('\n');
             }
-        }
 
-        System.out.print(ans);
+            System.out.println(count);
+        }
 
         sc.close();
     }
