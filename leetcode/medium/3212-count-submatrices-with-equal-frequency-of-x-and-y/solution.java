6c274f0382c895a398bd03fd64939be530fa2e8b
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-11 20:48:23 +0530

    ✅ Count Submatrices With Equal Frequency of X and Y | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-11

---
+++ leetcode/medium/3212-count-submatrices-with-equal-frequency-of-x-and-y/solution.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int numberOfSubmatrices(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // balance = number of X - number of Y
+        int[][] balance = new int[m + 1][n + 1];
+
+        // xCount = number of X characters
+        int[][] xCount = new int[m + 1][n + 1];
+
+        int ans = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                int value = 0;
+                int x = 0;
+
+                if (grid[i - 1][j - 1] == 'X') {
+                    value = 1;
+                    x = 1;
+                } else if (grid[i - 1][j - 1] == 'Y') {
+                    value = -1;
+                }
+
+                // Prefix sum for X - Y
+                balance[i][j] =
+                    balance[i - 1][j]
+                    + balance[i][j - 1]
+                    - balance[i - 1][j - 1]
+                    + value;
+
+                // Prefix sum for number of X's
+                xCount[i][j] =
+                    xCount[i - 1][j]
+                    + xCount[i][j - 1]
+                    - xCount[i - 1][j - 1]
+                    + x;
+
+                // Equal X and Y + at least one X
+                if (balance[i][j] == 0 && xCount[i][j] > 0) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
