ec1c82278a7159754db1fcf29415b9e2e6b9e211
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-14 21:18:49 +0530

    ✅ Q3. Sum of Scores of Built Strings | Hard | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-08-14

---
+++ leetcode/hard/2223-sum-of-scores-of-built-strings/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long sumScores(String s) {
+
+        int n = s.length();
+        int[] z = new int[n];
+
+        int left = 0;
+        int right = 0;
+
+        long answer = n; // z[0] = n
+
+        for (int i = 1; i < n; i++) {
+
+            if (i <= right) {
+                z[i] = Math.min(right - i + 1, z[i - left]);
+            }
+
+            while (i + z[i] < n &&
+                   s.charAt(z[i]) == s.charAt(i + z[i])) {
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > right) {
+                left = i;
+                right = i + z[i] - 1;
+            }
+
+            answer += z[i];
+        }
+
+        return answer;
+    }
+}
