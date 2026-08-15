b129f19d16200e5318365a8a372de5d585c12f41
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-15 12:13:58 +0530

    ✅ House Robber | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 42.5 MB | 2026-08-15

--- leetcode/medium/0198-house-robber/solution.java
+++ leetcode/medium/0198-house-robber/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
 
-        int prev2 = 0; // Maximum money up to i-2
-        int prev1 = 0; // Maximum money up to i-1
+        int prev2 = 0; // Maximum money up to i - 2
+        int prev1 = 0; // Maximum money up to i - 1
 
         for (int money : nums) {
+
+            // Either skip this house or rob it
             int current = Math.max(prev1, prev2 + money);
 
             prev2 = prev1;
