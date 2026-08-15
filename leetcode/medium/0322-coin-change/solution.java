ed19678e4c1c63b5d411097f2c416830f365863f
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-15 12:10:25 +0530

    ✅ Coin Change | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-15

---
+++ leetcode/medium/0322-coin-change/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        int[] dp = new int[amount + 1];
+
+        // amount + 1 represents infinity/impossible
+        Arrays.fill(dp, amount + 1);
+
+        dp[0] = 0;
+
+        for (int current = 1; current <= amount; current++) {
+
+            for (int coin : coins) {
+
+                if (coin <= current) {
+                    dp[current] = Math.min(
+                        dp[current],
+                        dp[current - coin] + 1
+                    );
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
