c1e95748cd8caac81aee8ba4e6494e973d2246d4
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-15 12:06:03 +0530

    ✅ Unique Paths II | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.3 MB | 2026-08-15

---
+++ leetcode/medium/0063-unique-paths-ii/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[] dp = new int[n];
+
+        // Starting point
+        dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                // Obstacle blocks all paths
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } 
+                else if (j > 0) {
+                    // Paths from top + paths from left
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
