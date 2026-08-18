fc5a1365f32945009eff1c11bcb879d892233ed4
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-18 22:44:28 +0530

    ✅ Rotate Array | Medium | Java | Runtime: 4 ms (4.18%) | Memory: 268.7 MB | 2026-08-18

--- leetcode/medium/0189-rotate-array/solution.java
+++ leetcode/medium/0189-rotate-array/solution.java
@@ -5,17 +5,15 @@ class Solution {
 
         reverse(nums, 0, n - 1);
         reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        reverse(nums, k, n - 1); 
+    } 
+    private void reverse(int[] nums, int left, int right) { 
+        while (left < right) { 
+            int temp = nums[left]; 
+            nums[left] = nums[right]; 
+            nums[right] = temp; 
+            left++; 
+            right--; 
+        } 
     }
-
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-
-            left++;
-            right--;
-        }
-    }
-}
+}    
