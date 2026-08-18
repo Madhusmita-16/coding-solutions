33c8da5a6737d31987a75ce1f84222a068081458
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-18 20:22:51 +0530

    ✅ Wildcard Matching | Hard | Java | Runtime: 3 ms (79.09%) | Memory: 43.3 MB | 2026-08-18

---
+++ leetcode/hard/0044-wildcard-matching/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                int a = num1.charAt(i) - '0';
+                int b = num2.charAt(j) - '0';
+
+                int product = a * b;
+
+                int pos1 = i + j;
+                int pos2 = i + j + 1;
+
+                int sum = product + result[pos2];
+
+                result[pos2] = sum % 10;
+                result[pos1] += sum / 10;
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int digit : result) {
+            if (ans.length() == 0 && digit == 0) {
+                continue;
+            }
+
+            ans.append(digit);
+        }
+
+        return ans.toString();
+    }
+}
