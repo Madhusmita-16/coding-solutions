076c29816e70c09ac75aff0b7c0f5e3970f16ac0
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-27 14:39:45 +0530

    ✅ Q2. Product of Array Except Self | Medium | Java | Runtime: 0 ms | Memory: 43.1 MB | 2026-08-27

--- leetcode/medium/0238-product-of-array-except-self/solution.java
+++ leetcode/medium/0238-product-of-array-except-self/solution.java
@@ -3,14 +3,14 @@ class Solution {
         int n = nums.length;
         int[] answer = new int[n];
 
-        // Prefix products
+        // Store prefix product in answer
         answer[0] = 1;
 
         for (int i = 1; i < n; i++) {
             answer[i] = answer[i - 1] * nums[i - 1];
         }
 
-        // Suffix products
+        // Multiply by suffix product
         int suffix = 1;
 
         for (int i = n - 1; i >= 0; i--) {
