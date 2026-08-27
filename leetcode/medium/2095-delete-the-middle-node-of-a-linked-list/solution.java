a23d2684d5e5107cc47be014ce4e08c7fb23461f
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-27 13:54:13 +0530

    ✅ Delete the Middle Node of a Linked List | Medium | Java | Runtime: 3 ms (99.93%) | Memory: 202.4 MB | 2026-08-27

--- leetcode/medium/2095-delete-the-middle-node-of-a-linked-list/solution.java
+++ leetcode/medium/2095-delete-the-middle-node-of-a-linked-list/solution.java
@@ -1,22 +1,23 @@
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
 
-        // If there is only one node, delete it
+        // Only one node
         if (head.next == null) {
             return null;
         }
 
         ListNode slow = head;
         ListNode fast = head;
+        ListNode prev = null;
 
-        // slow will reach the node before the middle
-        while (fast.next != null && fast.next.next != null) {
+        while (fast != null && fast.next != null) {
+            prev = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // Delete the middle node
-        slow.next = slow.next.next;
+        // slow is the middle node
+        prev.next = slow.next;
 
         return head;
     }
