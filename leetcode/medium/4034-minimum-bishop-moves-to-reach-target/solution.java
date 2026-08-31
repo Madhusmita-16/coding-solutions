2e2a762ce24517e0b34a70e2afa045d2263b5ade
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-08-31 12:42:34 +0530

    ✅ Minimum Bishop Moves to Reach Target | Medium | Java | Runtime: 1 ms (100.00%) | Memory: 43.8 MB | 2026-08-31

--- leetcode/medium/4034-minimum-bishop-moves-to-reach-target/solution.java
+++ leetcode/medium/4034-minimum-bishop-moves-to-reach-target/solution.java
@@ -7,17 +7,13 @@ class Solution {
         int tr = target[0];
         int tc = target[1];
 
-        // Bishop can only stay on the same color
         if ((sr + sc) % 2 != (tr + tc) % 2) {
             return -1;
         }
 
-        // Same diagonal
         if (Math.abs(sr - tr) == Math.abs(sc - tc)) {
             return 1;
         }
-
-        // Same color but different diagonal -> always reachable in 2 moves
         return 2;
     }
 }
