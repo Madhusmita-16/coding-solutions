2984fc9efaa7e63427313c601db74f0019e02bf4
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-09-01 11:12:38 +0530

    ✅ Nth Fibonacci Using Recursion | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ gfg/basic/fibonacci-using-recursion/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int nthFibonacci(int n) {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        return a;
+    }
+}
