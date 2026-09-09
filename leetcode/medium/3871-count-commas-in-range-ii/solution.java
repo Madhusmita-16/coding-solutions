32b49f6c29154a55108c42e230aba87c5db12f3a
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-09-09 19:34:18 +0530

    ✅ Count Commas in Range II | Medium | Java | Runtime: 1 ms (99.36%) | Memory: 42.7 MB | 2026-09-09

---
+++ leetcode/medium/3871-count-commas-in-range-ii/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long countCommas(long n) {
+        long total = 0;
+        long start = 1000;
+        long commas = 1;
+
+        while (start <= n) {
+            long end = start * 1000 - 1;
+
+            if (n < end) {
+                end = n;
+            }
+
+            total += (end - start + 1) * commas;
+
+            start *= 1000;
+            commas++;
+        }
+
+        return total;
+    }
+}
