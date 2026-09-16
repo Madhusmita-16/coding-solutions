e79e9d017bc06c3984d0e6f3f85b3db5d833ad15
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-09-16 08:24:53 +0530

    ✅ Q3. Subrectangle Queries | Medium | Java | Runtime: 1 ms | Memory: 43 MB | 2026-09-16

---
+++ leetcode/medium/1476-subrectangle-queries/solution.java
@@ -0,0 +1,20 @@
+class SubrectangleQueries {
+
+    private final int[][] rectangle;
+
+    public SubrectangleQueries(int[][] rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int row = row1; row <= row2; row++) {
+            for (int col = col1; col <= col2; col++) {
+                rectangle[row][col] = newValue;
+            }
+        }
+    }
+
+    public int getValue(int row, int col) {
+        return rectangle[row][col];
+    }
+}
