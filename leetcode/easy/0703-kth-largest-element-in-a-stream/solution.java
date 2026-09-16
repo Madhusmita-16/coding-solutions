b9f3349deca69e0dbdc1a772fedb36cc206944c0
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-09-16 08:12:58 +0530

    ✅ Q1. Kth Largest Element in a Stream | Easy | Java | Runtime: 2 ms | Memory: 42.8 MB | 2026-09-16

---
+++ leetcode/easy/0703-kth-largest-element-in-a-stream/solution.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+
+    private final int k;
+    private final PriorityQueue<Integer> minHeap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+
+        // Keep only the k largest elements
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
+    }
+}
