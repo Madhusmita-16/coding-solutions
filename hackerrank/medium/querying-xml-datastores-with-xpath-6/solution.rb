ed4ab7600d9acfb4d60e3801ae9fad95c3eb4d12
Madhu Smita Mishra <madhusmita1016@gmail.com>
2026-09-18 21:53:04 +0530

    ✅ Querying XML Datastores with XPath - 6 | Medium | Ruby | Runtime: N/A | Memory: N/A | 2026-09-18

---
+++ hackerrank/medium/querying-xml-datastores-with-xpath-6/solution.rb
@@ -0,0 +1,12 @@
+require 'rexml/document'
+include REXML
+
+xmlText = "" 
+
+while line = gets()
+    xmlText += line
+end
+
+doc = Document.new xmlText
+
+puts doc.elements["collection/movie[contains(description, 'bit')]/@title"]
